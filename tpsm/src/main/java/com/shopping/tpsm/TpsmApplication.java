c008dbfc8532c20bbb42daac1951273ae104feda
eecczz <70041613+eecczz@users.noreply.github.com>
2024-09-18 21:59:43 +0900

    init
    init

---
+++ tpsm/src/main/java/com/shopping/tpsm/TpsmApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.tpsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpsmApplication.class, args);
+    }
+
+}
